9c7d533fcf0f10763573b7e8399baa67fd3d7dd2
Hansal Bachkaniwala <hansal7014@gmail.com>
2020-02-23 18:02:01 -0500

    Add CSS for Settings sub-options

--- app/assets/css/settingsPage_styles.js
+++ app/assets/css/settingsPage_styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const settingsPageStyles = StyleSheet.create({
     utilityButton: {
         backgroundColor: '#000000',
-        margin: 10,
+        margin: 20,
         padding: 20,
         alignItems: 'center',
         justifyContent: 'center',
@@ -32,6 +32,72 @@ const settingsPageStyles = StyleSheet.create({
     diagram: {
         alignSelf: "center",
         marginTop: "2%",
+    },
+    inputField: {
+        backgroundColor: '#F0F0F0',
+        borderColor: '#000000',
+        paddingTop: 2,
+        paddingBottom: 10,
+        alignSelf: 'stretch',
+        marginStart: 10,
+        marginEnd: 10,
+        shadowRadius: 5,
+        shadowColor: '#F0F0F0',
+        borderRadius: 50,
+        borderWidth: 2.5,
+        fontFamily: 'Quicksand',
+        fontSize: 30,
+        textAlign: "center",
+        textAlignVertical: "center",
+        color: 'black'
+    },
+    inputFieldError: {
+        backgroundColor: '#FF0000',
+        borderColor: '#FF0000'
+    },
+    label: {
+        fontFamily: 'Quicksand', 
+        color: '#505050', 
+        margin: 20, 
+        fontSize: 24
+    },
+    textField: {
+        backgroundColor: '#F0F0F0',
+        borderColor: '#000000',
+        paddingTop: 2,
+        paddingBottom: 10,
+        alignSelf: 'stretch',
+        marginStart: 10,
+        marginEnd: 10,
+        shadowRadius: 5,
+        shadowColor: '#F0F0F0',
+        borderRadius: 20,
+        borderWidth: 2.5,
+        fontFamily: 'Quicksand',
+        fontSize: 30,
+        textAlign: "center",
+        textAlignVertical: "center",
+        color: 'black'
+    },
+    passwordInputField: {
+        backgroundColor: '#F0F0F0',
+        borderColor: '#000000',
+        paddingTop: 2,
+        paddingBottom: 10,
+        alignSelf: 'stretch',
+        marginStart: 10,
+        marginEnd: 10,
+        marginTop: 20,
+        marginBottom: 20,
+        shadowRadius: 5,
+        shadowColor: '#F0F0F0',
+        borderRadius: 50,
+        borderWidth: 2.5,
+        fontFamily: 'Quicksand',
+        fontSize: 30,
+        textAlign: "center",
+        textAlignVertical: "center",
+        color: 'black'
     }
 });
 
