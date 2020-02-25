7bcae24381bd1a3980078760051fa93a6d9cc07e
Hansal Bachkaniwala <Hansal7014@gmail.com>
2020-02-24 22:45:10 -0500

    Fixed grammar as per review comments

--- app/components/About.js
+++ app/components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <ScrollView style={settingsPageStyles.settingOptionContainer}>
                 <Text style={settingsPageStyles.headingStyle}>Application Overview</Text>
                 <Text style={{fontFamily: 'Quicksand', color: '#505050', padding: 20, margin: 20, fontSize: 24}}>
-                    The Imagère application allow a user equipped with an Android 
+                    The Imagère application allows a user equipped with an Android 
                     cell or IOS phone to upload a picture of an animal and receive 
                     the probable categorization of the creature. The image below 
                     is an overview of the app.
