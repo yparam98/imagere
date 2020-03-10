e7fe7533a7e761c14be603ad2fffb17ba1f8207c
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-03-10 15:33:10 -0400

    added alignSelf prop to customButton. Updated button in Photo upload component.

--- app/components/Button.js
+++ app/components/Button.js
@@ -15,7 +15,7 @@ class UtilityButton extends Component {
 
     render() {
         return (
-            <TouchableOpacity style={{ borderWidth: 0, backgroundColor: this.state.color, borderRadius: 45, margin: 5, flexDirection: "row" }} onPress={this.state.pressAction}>
+            <TouchableOpacity style={{ borderWidth: 0, backgroundColor: this.state.color, borderRadius: 45, margin: 5, flexDirection: "row", alignSelf:"auto" }} onPress={this.state.pressAction}>
                 <Icon name={this.state.icon} color="white" size={18} containerStyle={{ justifyContent: "center", paddingLeft: 8 }} />
                 <Text style={{ fontFamily: "Quicksand", fontSize: 18, color: "white", paddingTop: 5, paddingBottom: 5, paddingLeft: 7, paddingRight: 15 }}>
                     {this.state.buttonTitle}
