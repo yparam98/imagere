5cdffad30ffb6e312ef78bf3d8f044cc3e446ef9
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-03-11 03:19:26 -0400

    somewhat works. need to think of way to quickly render lots of images without internal time.

--- app/components/Newsfeed.js
+++ app/components/Newsfeed.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import { Text, View, Image, TouchableHighlight, FlatList, ActivityIndicator } from 'react-native';
 import * as Font from 'expo-font';
 import axios from 'axios';
 import moment from 'moment';
 import newsfeedPageStyles from '../assets/css/newsfeedPage_styles';
+import NewsfeedRenderer from './NewsfeedRenderer';
 
-class NewsfeedPane extends Component {
+class NewsfeedPane extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -13,70 +14,43 @@ class NewsfeedPane extends Component {
             newsfeedDataObj: undefined,
             myData: [],
             myNav: this.props.navigation,
-            // pictureID: "",
-            // picturePath: "",
-            // metadata: {
-            //     id: "",
-            //     dateTaken: "",
-            //     locationTaken: "",
-            //     photographer: {
-            //         id: "",
-            //         firstName: "",
-            //         lastName: "",
-            //         description: "",
-            //         profilePicture: ""
-            //     },
-            // },
         };
     }
 
-    async componentDidMount() {
-        // await axios.get('http://myvmlab.senecacollege.ca:6746/newsfeed')
-        //     .then((response) => {
-        //         // console.log(response.data);
-        //         for (let pictureObj of response.data) {
-        //             if (pictureObj != undefined && pictureObj.metadata != undefined && pictureObj.categorization != undefined) {
-        //                 console.log(pictureObj.categorization.nnResult[0].label);
-        //                 this.state.myData.push(pictureObj);
-        //             }
-        //         }
-        //     })
-        //     .catch((err) => {
-        //         console.log("error retrieving data from API: " + err);
-        //     });
-    }
-
-    async initializeData() {
+    initializeData = async () => {
         await axios.get('http://myvmlab.senecacollege.ca:6746/newsfeed')
             .then((response) => {
-                // console.log(response.data);
                 for (let pictureObj of response.data) {
                     if (pictureObj != undefined && pictureObj.metadata != undefined && pictureObj.categorization != undefined) {
-                        this.state.myData.push(pictureObj);
+                        if (pictureObj.metadata.photographer != null) {
+                            this.state.myData.push(pictureObj);
+                        }
                     }
                 }
+                this.setState({
+                    dataLoaded: true
+                });
             })
             .catch((err) => {
                 console.log("error retrieving data from API: " + err);
             });
     }
 
-    async renderNewsfeed() {
-        await this.initializeData().then(() => {
-            this.setState({
-                dataLoaded: true
-            });
-        });
+    renderNewsfeed = async () => {
+        await this.initializeData();
     }
 
     render() {
-        this.renderNewsfeed();
+        if (!this.state.dataLoaded) {
+            this.renderNewsfeed();
+        }
+
         return (
             <View>
                 {
                     !this.state.dataLoaded ? (
                         <View style={{ padding: 25, alignSelf: "center" }}><ActivityIndicator size="large" color="purple" /></View>
-                    ) : 
+                    ) : <NewsfeedRenderer newsfeedData={this.state.myData} />
                 }
             </View>
         )
@@ -96,8 +70,6 @@ class Newsfeed extends Component {
 
     async componentDidMount() {
 
-        // console.log(this.props.navigation.getParam(currentUser, ""));
-
         await Font.loadAsync({
             'Quicksand': require('../assets/fonts/Quicksand-Regular.ttf'),
             'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
@@ -106,7 +78,6 @@ class Newsfeed extends Component {
 
         this.setState({
             fontLoaded: true,
-            // currentUser: this.props.navigation.getParam(currentUser, "")
         });
     }
 
@@ -124,78 +95,3 @@ class Newsfeed extends Component {
 }
 
 export default Newsfeed;
-
-/*
-
-  {
-        "_id": "5e587ae4af5cbe617197cab0",
-        "pathToPicture": "images/speciesPictures/e5d98ed3c599293e6533d1d597e973221f46aed544601219695c6c80c75c7ccb.jpg",
-        "approval": false,
-        "metadata": null,
-        "__v": 0
-    },
-    {
-        "_id": "5e5c63a4ea3d4c91a231a811",
-        "pathToPicture": "images/speciesPictures/913a7a4e5136a9bf3d07e43b4e219a178236790acd4d228b99691759c1d7a721.jpg",
-        "approval": false,
-        "metadata": {
-            "public": false,
-            "_id": "5e5c63a4ea3d4c91a231a810",
-            "dateTaken": "2020-03-02T01:38:44.356Z",
-            "locationTaken": "Toronto",
-            "photographer": {
-                "accountVerified": false,
-                "passwordResetToken": "JLAPHICJPNOFJEMC",
-                "_id": "5e2e75cc06b77c26821a813d",
-                "firstName": "Sukhbeer",
-                "lastName": "Dhillon",
-                "emailAddress": "ssdhillon20@myseneca.ca",
-                "securityQuestions": [
-                    {
-                        "_id": "5e2e75cc06b77c26821a813e",
-                        "question": "Where am I?"
-                    }
-                ],
-                "__v": 0,
-                "description": "Student Developer",
-                "profilePicture": "images/profilePictures/35f5fd1a4661b49c87ce76fad088bfbeec973ee90f90f06ba843adcec34b1249.jpg",
-                "tokenExpiryDate": "2020-03-11T21:29:00.000Z"
-            },
-            "__v": 0
-        },
-        "__v": 0,
-        "categorization": {
-            "_id": "5e5c63bfea3d4c91a231a812",
-            "nnResult": [
-                {
-                    "_id": "5e5c63bfea3d4c91a231a817",
-                    "label": "Shih-Tzu",
-                    "percentile": 79.4
-                },
-                {
-                    "_id": "5e5c63bfea3d4c91a231a816",
-                    "label": "Lhasa",
-                    "percentile": 2.4
-                },
-                {
-                    "_id": "5e5c63bfea3d4c91a231a815",
-                    "label": "affenpinscher",
-                    "percentile": 1.6
-                },
-                {
-                    "_id": "5e5c63bfea3d4c91a231a814",
-                    "label": "Pekinese",
-                    "percentile": 1
-                },
-                {
-                    "_id": "5e5c63bfea3d4c91a231a813",
-                    "label": "Tibetan_terrier",
-                    "percentile": 0.7
-                }
-            ],
-            "picture": "5e5c63a4ea3d4c91a231a811",
-            "__v": 0
-        }
-    }
-
-*/
--- app/components/NewsfeedRenderer.js
+++ app/components/NewsfeedRenderer.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
-import { FlatList, View, TouchableHighlight, Text } from "react-native";
+import { FlatList, View, TouchableHighlight, Text, Image } from "react-native";
 import newsfeedPageStyles from "../assets/css/newsfeedPage_styles";
 import axios from "axios";
+import moment from "moment";
+import { Buffer } from "buffer";
 
 class NewsfeedRenderer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            myData: this.props.newsfeedData
+            myData: this.props.newsfeedData.reverse()
         };
     }
 
     getPostDate(incomingDate) {
-        return <Text style={newsfeedPageStyles.dateText}>{moment.unix(incomingDate).fromNow()}</Text>
+        return <Text style={newsfeedPageStyles.dateText}>{moment(incomingDate).fromNow()}</Text>
     }
 
     async getSpeciesPicture(incomingPictureURL) {
         let speciesPicture = "";
 
         try {
-            let imageSrc = await axios.post(this.state.dataURL + "/retrieveFile", { "incomingURL": incomingProfilePicURL }, { responseType: 'arraybuffer' });
+            let imageSrc = await axios.post("http://myvmlab.senecacollege.ca:6746" + "/retrieveFile", { "incomingURL": incomingProfilePicURL }, { responseType: 'arraybuffer' });
             speciesPicture: "data:image/jpg;base64," + Buffer.Buffer.from(imageSrc.data, 'binary').toString('base64');
         } catch (error) {
             speciesPicture: "https://www.retailx.com/wp-content/uploads/2019/12/iStock-476085198.jpg";
@@ -33,20 +35,39 @@ class NewsfeedRenderer extends Component {
     }
 
     async getUserProfilePic(incomingProfilePicURL) {
-        let profilePicture = "";
+        // let profilePicture = "";
 
-        try {
-            let imageSrc = await axios.post(this.state.dataURL + "/retrieveFile", { "incomingURL": incomingProfilePicURL }, { responseType: 'arraybuffer' });
-            profilePicture: "data:image/jpg;base64," + Buffer.Buffer.from(imageSrc.data, 'binary').toString('base64');
-        } catch (error) {
-            profilePicture: "https://www.retailx.com/wp-content/uploads/2019/12/iStock-476085198.jpg";
-        }
+        // if (!incomingProfilePicURL.profilePicture || incomingProfilePicURL.profilePicture == null) {
+        //     profilePicture: "https://www.retailx.com/wp-content/uploads/2019/12/iStock-476085198.jpg";
+        // }
+        // else {
+        //     console.log(incomingProfilePicURL.profilePicture);
+        //     try {
+        //         let imageSrc = await axios.post("http://myvmlab.senecacollege.ca:6746" + "/retrieveFile", { "incomingURL": incomingProfilePicURL.profilePicture }, { responseType: 'arraybuffer' });
+        //         profilePicture: "data:image/jpg;base64," + Buffer.Buffer.from(imageSrc.data, 'binary').toString('base64');
+        //     } catch (error) {
+        //         profilePicture: "https://www.retailx.com/wp-content/uploads/2019/12/iStock-476085198.jpg";
+        //     }
+        // }
 
-        return <Image source={{ uri: profilePicture }} style={newsfeedPageStyles.userImage} />
+        // console.log("profilePicture uri: " + profilePicture);
+
+        return <Image source={{ uri: "https://www.retailx.com/wp-content/uploads/2019/12/iStock-476085198.jpg" }} style={newsfeedPageStyles.userImage} />
     }
 
     getUsername(incomingUserName) {
-        return <Text style={newsfeedPageStyles.userName}>{incomingUserName}</Text>
+        let firstName = "";
+        let lastName = "";
+
+        if (incomingUserName.firstName != null) {
+            firstName = incomingUserName.firstName;
+        }
+
+        if (incomingUserName.lastName != null) {
+            lastName = incomingUserName.lastName;
+        }
+
+        return <Text style={newsfeedPageStyles.userName}>{firstName + " " + lastName}</Text>
     }
 
     getLocation(incomingLocation) {
@@ -64,13 +85,13 @@ class NewsfeedRenderer extends Component {
                     <View style={newsfeedPageStyles.userTagDateContainer}>
                         <TouchableHighlight underlayColor='rgba(0,0,0,0.0)' onPress={() => this.onUserNamePress(item)}>
                             <View style={newsfeedPageStyles.userTag}>
-                                {this.getUserProfilePic(item.metadata.photographer.profilePicture)}
-                                {this.getUsername(item.metadata.photographer.firstName + item.metadata.photographer.lastName)}
+                                {this.getUserProfilePic(item.metadata.photographer)}
+                                {this.getUsername(item.metadata.photographer)}
                             </View>
                         </TouchableHighlight>
                         {this.getLocation(item.metadata.locationTaken)}
                     </View>
-                    {this.getSpeciesPicture(item.photoURL)}
+                    {/* {this.getSpeciesPicture(item.photoURL)} */}
                     <Text style={newsfeedPageStyles.identificationText}>Identified as: {this.getIdentification(item.categorization.nnResult[0].label)}</Text>
                     {this.getPostDate(item.metadata.dateTaken)}
                 </View>
@@ -79,3 +100,4 @@ class NewsfeedRenderer extends Component {
     }
 }
 
+export default NewsfeedRenderer;
