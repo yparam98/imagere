2fc4c074faf577f296c00aa7ade2dcb5c65212f0
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-04-03 16:16:16 -0400

    full to refresh implemented

--- app/components/Newsfeed.js
+++ app/components/Newsfeed.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { View, FlatList, ActivityIndicator } from 'react-native';
 import * as Font from 'expo-font';
 import axios from 'axios';
@@ -7,7 +7,7 @@ import NewsfeedPanel from './NewsfeedPanel';
 import { Overlay } from 'react-native-elements';
 import ProfileSampler from './ProfileSampler';
 
-class Newsfeed extends PureComponent {
+class Newsfeed extends Component {
     constructor(props) {
         super(props);
         this.handler = this.handler.bind(this);
@@ -18,7 +18,7 @@ class Newsfeed extends PureComponent {
             myNav: this.props.navigation,
             overlayVisible: false,
             selectedUser: {},
-        }        
+        }
     }
 
     handler(user) {
@@ -39,6 +39,14 @@ class Newsfeed extends PureComponent {
             'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
         });
 
+        this.refreshData();
+
+        this.setState({
+            fontLoaded: true,
+        });
+    }
+
+    async refreshData() {
         await axios.get('http://myvmlab.senecacollege.ca:6746/newsfeed')
             .then((response) => {
                 response.data.reverse();
@@ -58,41 +66,33 @@ class Newsfeed extends PureComponent {
                             dataLoaded: true,
                         });
                     }
-                }            
+                }
             })
             .catch((err) => {
                 console.log("error retrieving data from API: " + err);
             });
-
-        this.setState({
-            fontLoaded: true,
-        });
     }
 
     render() {
         return (
             <View style={{ flex: 1 }}>
                 <View style={newsfeedPageStyles.newsfeedView}>
-                    {
-                        this.state.dataLoaded && this.state.fontLoaded ? (
-                            <View>
-                                <FlatList data={this.state.myData} initialNumToRender={5} showsVerticalScrollIndicator={false} renderItem={({ item }) =>
-                                    <NewsfeedPanel userObj={item} handler={this.handler}/>
-                                } keyExtractor={item => item._id} />
+                    <View>
+                        <FlatList data={this.state.myData} initialNumToRender={5} showsVerticalScrollIndicator={false} refreshing={!this.state.dataLoaded} onRefresh={() => { this.setState({ dataLoaded: false }); this.refreshData(); }} renderItem={({ item }) =>
+                            <NewsfeedPanel userObj={item} handler={this.handler} />
+                        } keyExtractor={item => item._id} />
 
-                                <Overlay
-                                    isVisible={this.state.overlayVisible}
-                                    onBackdropPress={() => { this.setState({ overlayVisible: false }) }}
-                                    animationType={"slide"}
-                                    transparent={true}
-                                    width={"90%"}
-                                    height={"90%"}
-                                    overlayBackgroundColor={"rgb(0,0,0)"}>
-                                    <ProfileSampler myUser={this.state.selectedUser}/>
-                                </Overlay>
-                            </View>
-                        ) : <ActivityIndicator size={"large"} />
-                    }
+                        <Overlay
+                            isVisible={this.state.overlayVisible}
+                            onBackdropPress={() => { this.setState({ overlayVisible: false }) }}
+                            animationType={"slide"}
+                            transparent={true}
+                            width={"90%"}
+                            height={"90%"}
+                            overlayBackgroundColor={"rgb(0,0,0)"}>
+                            <ProfileSampler myUser={this.state.selectedUser} />
+                        </Overlay>
+                    </View>                    
                 </View>
             </View>
         );
